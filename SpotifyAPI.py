dd3666dfeee53f80d0ed3ba975e30b35362d0f45
Noah Boecker <noah@Noahs-Windows-Phone.local>
2019-12-14 16:44:18 -0600

    refactored url creation

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -3,8 +3,16 @@ import spotipy.util as util
 import configparser
 
 seed_artist = "Tame Impala"
-BASE_URL = "https://api.spotify.com/v1/"
 
+BASE_URL = "https://api.spotify.com/v1"
+
+artist_search_query = "/search?q={}&type=artist"
+related_artist_query = "/artists/{}/related-artists"
+artist_name_from_id_query = "/artists/{}"
+albums_from_artist_id_query = "/artists/{}/albums"
+tracks_from_album_id_query = "/albums/{}/tracks"
+playlists_for_user_id_query = "/users/{}/playlists"
+current_user_query = "/me"
 
 class SpotifyApi:
     def __init__(self, username, scope, client_id, client_secret, redirect_uri):
@@ -12,40 +20,38 @@ class SpotifyApi:
         self.__headers = {"Authorization": "Bearer {}".format(token)}
 
     def artist_search(self, artist):
-        artist_query_string = "%20".join(artist.split(" "))
-        full_query_string = "{}search?q={}&type=artist".format(BASE_URL, artist_query_string)
-        artist_response =  self.__get(full_query_string)
-        artists = artist_response['artists']['items']
-        return artists[0]["id"]
+        artist_search_text = "%20".join(artist.split(" "))
+        query = build_query(artist_search_query, artist_search_text)
+        artist_response = self.__get(query)
+        return parse_out_first_artist_id(artist_response)
 
     def artist_related_artists(self, artist_id):
-        query_string = "{}artists/{}/related-artists".format(BASE_URL, artist_id)
-        res = self.__get(query_string)
+        query = build_query(related_artist_query, artist_id)
+        res = self.__get(query)
         return parse_out_related_artists(res)
 
     def artist_name_from_artist_id(self, artist_id):
-        query_string = "{}artists/{}".format(BASE_URL, artist_id)
-        res = self.__get(query_string)
+        query = build_query(artist_name_from_id_query, artist_id)
+        res = self.__get(query)
         return res['name']
 
     def artist_albums_from_artist_id(self, artist_id):
-        query_string = "{}artists/{}/albums".format(BASE_URL, artist_id)
-        res = self.__get(query_string)
+        query = build_query(albums_from_artist_id_query, artist_id)
+        res = self.__get(query)
         return parse_out_albums(res)
 
     def get_tracks_from_album_id(self, album_id):
-        query_string = "{}albums/{}/tracks".format(BASE_URL, album_id)
-        res = self.__get(query_string)
+        query = build_query(tracks_from_album_id_query, album_id)
+        res = self.__get(query)
         return parse_out_tracks(res)
 
     def user_playlists_from_user_id(self, user_id):
-        query_string = "{}users/{}/playlists".format(BASE_URL, user_id)
-        res = self.__get(query_string)
+        query = build_query(playlists_for_user_id_query, user_id)
+        res = self.__get(query)
         return parse_out_playlists(res)
 
     def get_current_user_id(self):
-        query_string = "{}me".format(BASE_URL)
-        res = self.__get(query_string)
+        res = self.__get(build_query(current_user_query))
         return res['id']
 
     def __get(self, url):
@@ -75,6 +81,15 @@ def __spotify_response_json_parser(response, key):
     return [(item['id'], item['name']) for item in response[key]]
 
 
+def parse_out_first_artist_id(artist_response):
+    artists = artist_response['artists']['items']
+    return artists[0]["id"]
+
+
+def build_query(query_extension, query_parameters=None):
+    return BASE_URL + (query_extension.format(query_parameters) if query_parameters else query_extension)
+
+
 def read_config():
     config = configparser.ConfigParser()
     config.read(".config")
@@ -84,6 +99,8 @@ def read_config():
     redirect_url = config.get("DEFAULT", "redirect_url")
 
     return username, client_id, client_secret, redirect_url
+
+# ---------- Demos --------------------
 
 
 def demo_everything():
@@ -117,8 +134,10 @@ def display_demo(description, demo_block_result):
 
     return demo_block_result
 
+
 def main():
     demo_everything()
+
 
 if __name__ == "__main__":
     main()
